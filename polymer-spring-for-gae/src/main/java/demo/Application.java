00358dfe10b06ce148807d556d0e5d0e2c1558ca
Simon Harding <simon.harding@businessdecision.com>
2017-02-09 16:23:19 +0100

    Work in progress...

--- polymer-spring-for-gae/src/main/java/demo/Application.java
+++ polymer-spring-for-gae/src/main/java/demo/Application.java
@@ -6,11 +6,10 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 
 @Configuration
 @ComponentScan
@@ -42,4 +41,43 @@ public class Application {
         model.put("content", "This is the content");
         return model;
     }
+
+    @RequestMapping(path = "/stockprices", method = RequestMethod.GET)
+    public List<String> stockPrices() {
+        return Arrays.asList("One", "Two", "Three");
+    }
+
+//
+//    /**
+//     * Escape an html string. Escaping data received from the client helps to
+//     * prevent cross-site script vulnerabilities.
+//     *
+//     * @param html the html string to escape
+//     * @return the escaped string
+//     */
+//    private String escapeHtml(String html) {
+//        if (html == null) {
+//            return null;
+//        }
+//        return html.replaceAll("&", "&amp;").replaceAll("<", "&lt;").replaceAll(">", "&gt;");
+//    }
+//
+//    @Override
+//    public StockPrice[] getPrices(String[] symbols) throws DelistedException {
+//        List<StockPrice> prices = new ArrayList<StockPrice>(symbols.length);
+//
+//        Random rnd = new Random();
+//        for (String nextSymbol : symbols) {
+//            if ("ERR".equals(nextSymbol)) {
+//                throw new DelistedException(nextSymbol);
+//            }
+//            double price = rnd.nextDouble() * MAX_PRICE;
+//            double change = price * MAX_PRICE_CHANGE * (rnd.nextDouble() * 2.0 - 1.0);
+//
+//            prices.add(new StockPrice(nextSymbol, price, change));
+//        }
+//
+//        return prices.toArray(new StockPrice[]{});
+//    }
+
 }
