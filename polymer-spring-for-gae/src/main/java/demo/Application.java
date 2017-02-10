bcc77f9adb96481841172144c1c7537d2723ae39
Simon Harding <simon.harding@businessdecision.com>
2017-02-10 11:43:56 +0100

    Work in progress

--- polymer-spring-for-gae/src/main/java/demo/Application.java
+++ polymer-spring-for-gae/src/main/java/demo/Application.java
@@ -3,6 +3,7 @@ package demo;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -43,41 +44,33 @@ public class Application {
     }
 
     @RequestMapping(path = "/stockprices", method = RequestMethod.GET)
-    public List<String> stockPrices() {
-        return Arrays.asList("One", "Two", "Three");
+    public List<StockPrice> stockPrices() {
+        final double MAX_PRICE = 100.0; // $100.00
+        final double MAX_PRICE_CHANGE = 0.02; // +/- 2%
+
+        Random rand = new Random(new Date().getTime());
+        int numPrices = rand.nextInt(15);
+        List<StockPrice> result = new ArrayList<StockPrice>();
+        for (int i = 0; i < numPrices; i++) {
+            double price = rand.nextDouble() * MAX_PRICE;
+            double change = price * MAX_PRICE_CHANGE * (rand.nextDouble() * 2.0 - 1.0);
+
+            result.add(new StockPrice("Symbol " + i, price, change));
+        }
+        return result;
     }
 
-//
-//    /**
-//     * Escape an html string. Escaping data received from the client helps to
-//     * prevent cross-site script vulnerabilities.
-//     *
-//     * @param html the html string to escape
-//     * @return the escaped string
-//     */
-//    private String escapeHtml(String html) {
-//        if (html == null) {
-//            return null;
-//        }
-//        return html.replaceAll("&", "&amp;").replaceAll("<", "&lt;").replaceAll(">", "&gt;");
-//    }
-//
-//    @Override
-//    public StockPrice[] getPrices(String[] symbols) throws DelistedException {
-//        List<StockPrice> prices = new ArrayList<StockPrice>(symbols.length);
-//
-//        Random rnd = new Random();
-//        for (String nextSymbol : symbols) {
-//            if ("ERR".equals(nextSymbol)) {
-//                throw new DelistedException(nextSymbol);
-//            }
-//            double price = rnd.nextDouble() * MAX_PRICE;
-//            double change = price * MAX_PRICE_CHANGE * (rnd.nextDouble() * 2.0 - 1.0);
-//
-//            prices.add(new StockPrice(nextSymbol, price, change));
-//        }
-//
-//        return prices.toArray(new StockPrice[]{});
-//    }
-
+    /**
+     * Escape an html string. Escaping data received from the client helps to
+     * prevent cross-site script vulnerabilities.
+     *
+     * @param html the html string to escape
+     * @return the escaped string
+     */
+    private String escapeHtml(String html) {
+        if (html == null) {
+            return null;
+        }
+        return html.replaceAll("&", "&amp;").replaceAll("<", "&lt;").replaceAll(">", "&gt;");
+    }
 }
