fc0a19b1869e5ae8b3defffa5ebba136287f9958
Simon Harding <simon.harding@bdch.local>
2017-02-17 11:10:38 +0100

    Work in Progress

--- polymer-spring-for-gae/src/main/java/demo/TimeTrack.java
+++ polymer-spring-for-gae/src/main/java/demo/TimeTrack.java
@@ -1,28 +1,34 @@
 package demo;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.util.Date;
 
 /**
  * Created by simon.harding on 15.02.2017.
  */
 public class TimeTrack {
-    private String m_from;
-    private String m_to;
 
-    public String getFrom() {
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm")
+    private Date m_from;
+
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm")
+    private Date m_to;
+
+    public Date getFrom() {
         return m_from;
     }
 
-    public void setFrom(String from) {
+    public void setFrom(Date from) {
         this.m_from = from;
     }
 
-    public String getTo() {
+    public Date getTo() {
         return m_to;
     }
 
-    public void setTo(String to) {
+    public void setTo(Date to) {
         this.m_to = to;
     }
 }
